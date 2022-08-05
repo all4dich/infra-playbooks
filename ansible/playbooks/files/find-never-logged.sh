8b9ec1c99f96b3d813739663a018468fd2685436
Sunjoo Park <sunjoo.park@nota.ai>
2022-08-05 15:28:57 +0900

    ansible: Check a user's home directory size when Never logged

--- ansible/playbooks/files/find-never-logged.sh
+++ ansible/playbooks/files/find-never-logged.sh
@@ -5,5 +5,17 @@ USERS=$(cat /etc/passwd|awk -F: '{print $1","$3}'|grep -E '\,[0-9]{4}$')
 HOSTNAME=$(hostname)
 for a in ${USERS}
 do
-    echo $HOSTNAME,$a
+    IFS=',' read -r -a USER_INFO <<< "$a"
+    USER_NAME=${USER_INFO[0]}
+    USER_UID=${USER_INFO[1]}
+    # Check if user has never logged onto a machine
+    if [ "${USER_NAME}" = "docker" ] || [ "${USER_NAME}" = "mysql" ]; then
+        CHECK_LOGIN=0
+    else
+        CHECK_LOGIN=`lastlog -u ${USER_NAME}|grep Never\ logged|wc -l`
+    fi
+    if [ "${CHECK_LOGIN}" = 1 ]; then
+        HOME_SIZE=`bash -c "du -sh ~${USER_NAME}"|tr '\t' ','`
+        echo ${HOSTNAME},${USER_NAME},${USER_UID},${HOME_SIZE}
+    fi    
 done
