6cb9f039e5600639ff3d7f9e3d1493e7ca136bc1
hyeonhoSin <hyeonho.sin@nota.ai>
2023-10-04 20:43:55 +0900

    사용자 계정 정보 수집 목록을 Server, Username, UID, GID, Groups, Home_dir, Last_Login, account_lock, account_del 항목으로 세분화 하여 수집하는 기능을 추가하였습니다. 90일 이상 미 접속시에는 account_lock이 Y로 계정 비활성화 대상이며, 365일 이상 미접속하거나 한번도 접속하지 않은 계정은 account_del이 Y로 계정 삭제 대상입니다. 실제 비활성화 및 삭제 기능은 아직 구현하지 않았습니다.

--- ansible/playbooks/files/find-user-accounts.sh
+++ ansible/playbooks/files/find-user-accounts.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-# Get a list of registered users
-USERS=$(cat /etc/passwd|awk -F: '{print $1","$3}'|grep -E '\,[0-9]{4}$')
-HOSTNAME=$(hostname)
-for a in ${USERS}
-do
-    IFS=',' read -r -a USER_INFO <<< "$a"
-    USER_NAME=${USER_INFO[0]}
-    USER_UID=${USER_INFO[1]}
-    # Check if user has never logged onto a machine
-    CHECK_LOGIN=`LC_ALL=C lastlog -u ${USER_NAME}|tail -n 1`
-    CONN_INFO="Never Logged"
-    if [[ "${CHECK_LOGIN}" =~ [0-9]$ ]]; then
-        CONN_INFO=`echo $CHECK_LOGIN|sed "s/${USER_NAME} //g"`
+echo "Server,Username,UID,GID,Groups,Home_Directory,Last_Login,account_lock,account_del"
+
+while IFS=: read -r username _ uid gid _ home shell; do
+    if [ "$uid" ] && [ "$uid" -ge 1000 ] && [ "$shell" != "/usr/sbin/nologin" ]; then
+        groups=$(id -Gn "$username" | tr '\n' ',' | sed 's/,$//')  # Remove the trailing comma
+
+        # Check last login date
+        last_login_raw=$(lastlog -u "$username" | grep -v "Username")
+        last_login=$(echo "$last_login_raw" | awk '{print $2}')
+        if [ "$last_login" = "**Never" ]; then
+            last_login="No Login"
+            account_lock="Y"
+            account_del="Y"
+        else
+            last_login=$(echo "$last_login_raw" | awk '{print $9, $5, $6}')
+            last_login=$(lastlog -u "$username" | grep -v "Username" | awk '{gsub(/월/, "", $5); print $9"-"$5"-"$6}' | sed 's/Jan/01/;s/Feb/02/;s/Mar/03/;s/Apr/04/;s/May/05/;s/Jun/06/;s/Jul/07/;s/Aug/08/;s/Sep/09/;s/Oct/10/;s/Nov/11/;s/Dec/12/' | xargs -I{} date --date="{}" +"%Y-%m-%d")
+            account_lock="N"
+            account_del="N"
+        fi
+
+        # Set account lock and delete based on last login date
+        if [ "$last_login" != "No Login" ]; then
+            today=$(date '+%Y-%m-%d')
+            last_login_timestamp=$(date -d "$last_login" '+%s')
+            today_timestamp=$(date -d "$today" '+%s')
+
+            days_difference=$(( (today_timestamp - last_login_timestamp) / 86400 ))
+
+            if [ "$days_difference" -gt 90 ]; then
+                account_lock="Y"
+            fi
+
+            if [ "$days_difference" -gt 365 ]; then
+                account_del="Y"
+            fi
+        fi
+
+        echo "$HOSTNAME,$username,$uid,$gid,$groups,$home,$last_login,$account_lock,$account_del"
     fi
-    echo ${HOSTNAME},${USER_NAME},${CONN_INFO}
-done
+done < /etc/passwd
+
