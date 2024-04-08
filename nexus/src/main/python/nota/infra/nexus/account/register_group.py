f5b74ee906c24d3996811c04ce0bbd26bc0edf8f
Sunjoo Park <all4dich@gmail.com>
2024-04-08 12:06:56 -0700

    nexus: Create coworking docker registry path
    :Detaild Notes:
        - Create coworking docker registry path
        - Two or more people can upload and updated container images with
          the specific path
        - People who have to manage container images has a role
          'nota/group/GROUP_NAME'
        - Create a group
          - pytyhon3 nota/infra/nexus/account/register_group.py --username=sunjoo.park --password=xxxxxx --url=https://registry.nota.ai --target_group=muirwoods --target_user=keonyul.park
          - Create a configuration to manage container images under nota/group/muirwoods/
          - If a user need a right to manage container images, 'nota/group/GROUP_NAME' role has to be given to a user
    
    :Issues:
    IM-309
    IM-312

--- nexus/src/main/python/nota/infra/nexus/account/register_group.py
+++ nexus/src/main/python/nota/infra/nexus/account/register_group.py
@@ -2,6 +2,7 @@ import requests
 import argparse
 import os
 import logging
+import json
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -10,7 +11,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--username", required=True)
 parser.add_argument("--password", required=True)
 parser.add_argument("--url", required=True)
-parser.add_argument("--target_user", required=True)
+parser.add_argument("--target_group")
+parser.add_argument("--target_user")
 args = parser.parse_args()
 nexus_username = args.username
 nexus_password = args.password
@@ -31,10 +33,10 @@ def get_privileges():
     print(r.text)
 
 
-def create_content_selector_for_docker_personal(target_user_name):
+def create_content_selector_for_docker_group(target_group_name):
     url = f"{nexus_url}/service/rest/v1/security/content-selectors"
-    cs_name = f"docker-nota-people-{target_user_name}"
-    cs_expression = f"format == \"docker\" and path =^ \"/v2/nota/people/{target_user_name}/\""
+    cs_name = f"docker-nota-group-{target_group_name}"
+    cs_expression = f"format == \"docker\" and path =^ \"/v2/nota/group/{target_group_name}/\""
     data = {
         "name": cs_name,
         "description": cs_name,
@@ -42,9 +44,9 @@ def create_content_selector_for_docker_personal(target_user_name):
         "enabled": True
     }
     r = requests.post(url, json=data, auth=(nexus_username, nexus_password))
-    logging.info(f"Status code for creating content selector for {target_user_name}: {r.status_code}")
+    logging.info(f"Status code for creating content selector for {target_group_name}: {r.status_code}")
     if r.status_code in [200, 201, 204]:
-        logging.info(f"Successfully created content selector for {target_user_name}")
+        logging.info(f"Successfully created content selector for {target_group_name}")
         return cs_name
     else:
         logging.error(r.text)
@@ -52,16 +54,16 @@ def create_content_selector_for_docker_personal(target_user_name):
 
 
 # Create privilege for the user
-def create_privilege_for_docker_personal(target_user_name):
+def create_privilege_for_docker_group(target_user_name):
     url = f"{nexus_url}/service/rest/v1/security/privileges/repository-content-selector"
-    privilege_name = f"docker-nota-people-{target_user_name}"
+    privilege_name = f"docker-nota-group-{target_user_name}"
     data = {
         "name": privilege_name,
         "description": privilege_name,
         "actions": ["ALL"],
         "format": "docker",
         "repository": "*",
-        "contentSelector": f"docker-nota-people-{target_user_name}",
+        "contentSelector": privilege_name
     }
     r = requests.post(url, json=data, auth=(nexus_username, nexus_password))
     logging.info(f"Status code for creating privilege for {target_user_name}: {r.status_code}")
@@ -74,35 +76,35 @@ def create_privilege_for_docker_personal(target_user_name):
 
 
 # Register account on nexus repository
-def register_account(target_username):
-    cs_name = create_content_selector_for_docker_personal(target_username)
+def register_group(target_groupname, target_username="sunjoo.park"):
+    cs_name = create_content_selector_for_docker_group(target_groupname)
     if cs_name is None:
-        logging.error(f"Failed to create content selector for {target_username}")
+        logging.error(f"Failed to create content selector for {target_groupname}")
         return
-    logging.info(f"Successfully created content selector for {target_username}")
-    privilage_name = create_privilege_for_docker_personal(target_username)
+    logging.info(f"Successfully created content selector for {target_groupname}")
+    privilage_name = create_privilege_for_docker_group(target_groupname)
     if privilage_name is None:
-        logging.error(f"Failed to create privilege for {target_username}")
+        logging.error(f"Failed to create privilege for {target_groupname}")
         return
-    logging.info(f"Successfully created privilege for {target_username}")
-    role_name = create_role_for_docker_personal(target_username)
+    logging.info(f"Successfully created privilege for {target_groupname}")
+    role_name = create_role_for_docker_group(target_groupname)
     if role_name is None:
-        logging.error(f"Failed to create role for {target_username}")
+        logging.error(f"Failed to create role for {target_groupname}")
         return
-    logging.info(f"Successfully created role for {target_username}")
-    returned_username = update_role_for_user(target_username)
+    logging.info(f"Successfully created role for {target_groupname}")
+    returned_username = add_user_to_group(target_groupname, target_username)
     if returned_username is None:
-        logging.error(f"Failed to update role for {target_username}")
+        logging.error(f"Failed to update role for {target_groupname}")
         return
-    logging.info(f"Successfully updated role for {target_username}")
+    logging.info(f"Successfully updated role for {target_groupname}")
 
 
 # Create nexus role for the user
-def create_role_for_docker_personal(target_user_name):
+def create_role_for_docker_group(target_user_name):
     url = f"{nexus_url}/service/rest/v1/security/roles"
-    role_name = f"nota-people-{target_user_name}"
-    role_id = f"nota-people-{target_user_name}"
-    privileges_ids = [f"docker-nota-people-{target_user_name}"]
+    role_name = f"nota-group-{target_user_name}"
+    role_id = role_name
+    privileges_ids = [f"docker-nota-group-{target_user_name}"]
     added_roles_ids = ["nota-common"]
     data = {
         "id": role_id,
@@ -122,19 +124,30 @@ def create_role_for_docker_personal(target_user_name):
 
 
 # Update nexus role for the user
-def update_role_for_user(target_user_name, account_source="LDAP"):
-    url = f"{nexus_url}/service/rest/v1/security/users/{target_user_name}"
-    user_roles = [f"nota-people-{target_user_name}", "nota-common"]
+def add_user_to_group(target_group_name, target_user_name, account_source="LDAP"):
+    group_role_name = f"nota-group-{target_group_name}"
+    # Get user roles from user information
+    url = f"{nexus_url}/service/rest/v1/security/users/?userId={target_user_name}"
+    r = requests.get(url, auth=(nexus_username, nexus_password))
+    logging.info(f"Status code for getting user information for {target_user_name}: {r.status_code}")
+    if r.status_code not in [200, 201, 204]:
+        logging.error(r.text)
+        logging.error(f"Failed to get user information for {target_user_name}")
+        return None
+    user_info = json.loads(r.text)[0]
+    user_roles = user_info["roles"]
+    user_roles.append(group_role_name)
     data = {
         "userId": target_user_name,
         "source": account_source,
         "roles": user_roles,
         "lastName": target_user_name.split(".")[1],
         "firstName": target_user_name.split(".")[0],
-        "emailAddress": target_user_name + "@nota.ai",
+        "emailAddress": user_info["emailAddress"],
         "status": "active"
     }
-    r = requests.put(url, json=data, auth=(nexus_username, nexus_password))
+    url_for_updating_user = f"{nexus_url}/service/rest/v1/security/users/{target_user_name}"
+    r = requests.put(url_for_updating_user, json=data, auth=(nexus_username, nexus_password))
     logging.info(f"Status code for updating role for {target_user_name}: {r.status_code}")
     if r.status_code in [200, 201, 204]:
         logging.info(f"Successfully updated role for {target_user_name}")
@@ -144,28 +157,9 @@ def update_role_for_user(target_user_name, account_source="LDAP"):
         return None
 
 
-def clear_account(user_name):
-    # Remove all roles from the user
-    url = f"{nexus_url}/service/rest/v1/security/users/{user_name}"
-    data = {
-        "userId": user_name,
-        "source": "LDAP",
-        "roles": ["nx-anonymous"],
-        "lastName": user_name.split(".")[1],
-        "firstName": user_name.split(".")[0],
-        "emailAddress": user_name + "@nota.ai",
-        "status": "active"
-    }
-
-    r = requests.put(url, json=data, auth=(nexus_username, nexus_password))
-    logging.info(f"Status code for removing roles from {user_name}: {r.status_code}")
-    if r.status_code in [200, 201, 204]:
-        logging.info(f"Successfully removing roles from {user_name}")
-    else:
-        logging.error(r.text)
-
-    # Delete role for the user
-    target_role_name = f"nota-people-{user_name}"
+def clear_group(group_name):
+    # Delete role for the group
+    target_role_name = f"nota-group-{group_name}"
     url = f"{nexus_url}/service/rest/v1/security/roles/{target_role_name}"
     r = requests.delete(url, auth=(nexus_username, nexus_password))
     logging.info(f"Status code for deleting role for {target_role_name}: {r.status_code}")
@@ -174,30 +168,32 @@ def clear_account(user_name):
     else:
         logging.error(r.text)
 
-    # Delete privilege and content selector for the user
-    target_username = f"docker-nota-people-{user_name}"
-    url = f"{nexus_url}/service/rest/v1/security/privileges//{target_username}"
+    # Delete privilege and content selector for the group
+    target_groupname = f"docker-nota-group-{group_name}"
+    url = f"{nexus_url}/service/rest/v1/security/privileges//{target_groupname}"
     r = requests.delete(url, auth=(nexus_username, nexus_password))
-    logging.info(f"Status code for deleting privilege for {target_username}: {r.status_code}")
+    logging.info(f"Status code for deleting privilege for {target_groupname}: {r.status_code}")
     if r.status_code in [200, 201, 204]:
-        logging.info(f"Successfully deleted privilege for {target_username}")
+        logging.info(f"Successfully deleted privilege for {target_groupname}")
     else:
         logging.error(r.text)
 
-    url = f"{nexus_url}/service/rest/v1/security/content-selectors/{target_username}"
+    url = f"{nexus_url}/service/rest/v1/security/content-selectors/{target_groupname}"
     r = requests.delete(url, auth=(nexus_username, nexus_password))
-    logging.info(f"Status code for deleting content selector for {target_username}: {r.status_code}")
+    logging.info(f"Status code for deleting content selector for {target_groupname}: {r.status_code}")
     if r.status_code in [200, 201, 204]:
-        logging.info(f"Successfully deleted content selector for {target_username}")
+        logging.info(f"Successfully deleted content selector for {target_groupname}")
     else:
         logging.error(r.text)
-
-
-
 
 
 if __name__ == "__main__":
     # get_privileges()
     # get_content_selectors()
-    register_account(args.target_user)
-    #clear_account("keonyul.park")
+    target_group_name = args.target_group
+    target_user_name = args.target_user
+
+    #register_group("test-group", "sunjoo.park")
+    #register_group(target_group_name, target_user_name)
+    add_user_to_group(target_group_name, "sunjoo.park")
+    #clear_group("test-group")
