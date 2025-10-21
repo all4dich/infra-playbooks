3eb1a44a0b6b496c7452a1c1520e6c8d33c916ba
Sunjoo Park <all4dich@gmail.com>
2025-10-21 15:55:27 +0900

    feat(ansible): rename parameters in call_client module and update example playbook

--- ansible-galaxy/playbooks/roles/check_linux_system/library/call_client.py
+++ ansible-galaxy/playbooks/roles/check_linux_system/library/call_client.py
@@ -2,10 +2,12 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
-
 def main():
     # Define any arguments your module might accept (even if none)
-    module_args = dict()
+    module_args = dict(
+        user_name=dict(type='str', required=True),
+        user_location=dict(type='str', required=False, default='default_value')
+    )
     # Create the module object
     module = AnsibleModule(
         argument_spec=module_args,
@@ -14,13 +16,17 @@ def main():
 
     # --- Your module's logic goes here ---
     # For this example, we just set a message.
+    p1 = module.params['user_name']
+    p2 = module.params['user_location']
 
     # -------------------------------------
 
     # Prepare the result dictionary to send back
     result = {
         "changed": False,  # Set to True if your module made a change
-        "msg": "Hello from call_client.py"
+        "msg": "Hello from call_client.py : " + p1,
+        "user_name": p1,
+        "user_location": p2
     }
 
     # This function correctlyf ormats and exits with the JSON result
